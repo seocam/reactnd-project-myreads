100f46e96c25d2ae760018a0a4e90985faa75f2a
Sergio Oliveira <seocam@seocam.com>
2017-10-07 18:23:17 -0300

    Added BrowserRoter wrapping main app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const app_code = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
+
+ReactDOM.render(app_code, document.getElementById('root'))
